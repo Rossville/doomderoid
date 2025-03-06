9ebf3f9a181c2a3f4b5dcb525768fed13d324080
Rossville <ashutosh.singh.9510@gmail.com>
2025-03-06 20:58:38 +0530

    project setup

---
+++ build/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.get('/user/:id', (req, res) => {
+    console.log(req.params.id);
+    res.status(200).json({
+        message: "get request received successfully!"
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+});
